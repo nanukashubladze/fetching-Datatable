94daec46b6c33bacd61ca7064aff722f64d90dd0
nanukashubladze <123475291+nanukashubladze@users.noreply.github.com>
2023-12-21 21:13:32 +0400

    done

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-console.log("first")
+ocument.addEventListener("DOMContentLoaded", function () {
+  const tableBody = document.getElementById("table-body");
+
+
+  const fetchAndPopulateTable = async () => {
+    try {
+      const response = await fetch("https://api.escuelajs.co/api/v1/products/");
+      const data = await response.json();
+      renderTable(data);
+      console.log(data)
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  };
+  
+
+ 
+  const renderTable = (products) => {
+    tableBody.innerHTML = "";
+    products.forEach((product) => {
+      const row = document.createElement("tr");
+      row.innerHTML = `
+        <td>${product.id}</td>
+        <td>${product.title}</td>
+        <td>${product.price}</td>
+        <td>${product.category.name}</td>
+        <td>${product.creationAt}</td>
+      `;
+      tableBody.appendChild(row);
+    });
+  };
+
+  fetchAndPopulateTable();
+});
